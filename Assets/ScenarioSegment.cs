a06cf37c8507f5dfbf5409e4a96a14dc663073c8
bazukax <33648731+bazukax@users.noreply.github.com>
2024-01-27 16:33:56 +0200

    Particles

---
+++ Assets/ScenarioSegment.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioSegment : MonoBehaviour
+{
+    public AudioClip mainVoiceover;
+    //shittalking
+
+    //mygtukai
+
+    //
+    //
+
+    // 5 main voiceovers - from full health - to low health
+    // 10 random taking a hit voiceovers;
+    // 10 random shit talk voiceovers;
+
+}
